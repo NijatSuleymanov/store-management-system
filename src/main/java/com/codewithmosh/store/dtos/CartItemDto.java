30a4883c80697f5f91290ccf1a240996e41ec846
Nicat <snicat894@gmail.com>
2025-05-30 21:54:23 +0400

    feat(cart): implement create, get, add item, and update item endpoints

---
+++ src/main/java/com/codewithmosh/store/dtos/CartItemDto.java
@@ -0,0 +1,14 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CartItemDto {
+
+    public CartProductDto product;
+    public int quantity;
+    public BigDecimal totalPrice;
+
+}
