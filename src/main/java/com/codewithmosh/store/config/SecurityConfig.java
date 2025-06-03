47f81aa6293e276138a414285652896895a4a93f
Nicat <snicat894@gmail.com>
2025-06-04 02:45:49 +0400

    Implement JWT-based authentication and secure login API

---
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.codewithmosh.store.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                //yeni sessiya saxlanılmır ->Token-based
+                .sessionManagement(c->
+                       c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //CSRF(Cross-Site Request Forgery -> "saytlar arası sorğu saxtakarlığı")
+                //qorunmasi söndürülür -> Çünki Stateless sistemlerde CSRF ehtiyac yoxdur
+               .csrf(AbstractHttpConfigurer::disable)
+                //Endpoint izacələri
+                .authorizeHttpRequests(c->
+                        //bütün sorğulara icazer verilirki login teleb olunmasin
+                        c.requestMatchers("/carts/**").permitAll()
+                                //usersde olan yalniz post metodu üçün login teleb etmir
+                                //ve auth loginde de teleb etmir biz icaze veririki girsin
+                               .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                               .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+                                //Diger bütün sorğular login teleb edir
+                                .anyRequest().authenticated()
+              );
+
+        return http.build();
+    }
+}
+
+
+
